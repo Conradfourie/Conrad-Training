56d06d31c2b57d40b240df64335b5ab29d10a6fa
Conradfourie <konradfourie@gmail.com>
2023-01-03 22:38:56 +0200

    Initial add to repository

---
+++ src/oop/Demo.java
@@ -0,0 +1,22 @@
+package oop;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Joe";
+		String email = "joe@testemail.com";
+		String phone = "0647537410";
+		
+		System.out.println(name + " is walking");
+		System.out.println(name + " is eating");
+		
+		String name2 = "Sarah";
+		String email2 = "Sarah@testemail.com";
+		String phone2 = "0647537410";
+		
+		
+		
+	}
+
+}
