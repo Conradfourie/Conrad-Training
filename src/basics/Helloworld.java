f088fe7fcd45464ed4c573fdbe146e115971e74e
Conradfourie <121899976+Conradfourie@users.noreply.github.com>
2023-01-04 22:12:20 +0200

    Update Helloworld.java

--- src/basics/Helloworld.java
+++ src/basics/Helloworld.java
@@ -5,7 +5,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("I'm adding code to my project");
-		system.out.println("Adding line from github");
+		System.out.println("Adding line from github");
 	}
 
 }
