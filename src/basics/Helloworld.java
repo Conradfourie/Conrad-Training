86f679f5a8c466d18628b8a657d11ad423fc4b9b
Conradfourie <konradfourie@gmail.com>
2023-01-04 22:03:36 +0200

    modified Helloword app

---
+++ src/basics/Helloworld.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("I'm adding code to my project");
+
+	}
+
+}
